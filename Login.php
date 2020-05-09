a81786c0efe3f8051d05ee2b8dd8069bd3186319
Shlomi Moreh <shlomi295@gmail.com>
2020-05-09 17:38:48 +1000

    Added bootstrap and stylesheet

--- Login.php
+++ Login.php
@@ -3,10 +3,16 @@
 
 <head>
     <title>Login Page</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="stylesheet.css">
 
     <php 
    include('Db.php');
 
+    
    $result = callDb('SELECT * FROM Login');
 
    
@@ -14,14 +20,22 @@
 ?>
 </head>
 
-<body> 
-    <p>Login page</p>
-    <p> 
-    <php
-    while(OCIFetch($result))
-   {
-       echo $result;
-   }
-     ?>
-    </p>
+<body>
+<div class="container">   
+<form action="login.php" method="POST">
+  <div class="form-group col-md-6 login-input">
+    <label class="col-form=lbl col-form-lbl-sm" for="exampleInputEmail1">Username</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+  </div>
+  <div class="form-group col-md-6 login-input">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+<div class="form-group" col-md-6>
+<button type="submit" class="btn btn-primary">Submit</button>
+
+</div>
+</form>
+</div>
 </body>
+
